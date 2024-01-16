c5f3a490a47f91b47ba0f3b2ede82930cbc30742
Daniel Puckowski <puckowski.d@gmail.com>
2024-01-16 17:50:11 -0500

    Proof of solution repository
    * Proof of solution repository with initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FirstComponent,
+    SecondComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
